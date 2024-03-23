f277f378213e581d3434f4f6aaed47b686ab208b
AtahanSHN <atahansahin2001@hotmail.com>
2024-03-24 01:49:06 +0300

    .

--- çokluÇekimDeneme.py
+++ çokluÇekimDeneme.py
@@ -1,5 +1,5 @@
 
-# -- coding: utf-8 --
+
 import pandas as pd
 from selenium import webdriver
 from selenium.webdriver.firefox.service import Service
@@ -10,7 +10,7 @@ from selenium.webdriver.support import expected_conditions as EC
 import time
 from urllib.parse import urlparse, parse_qs
 
-service = Service(executable_path='C:\\Users\\Hazar\\Desktop\\evfiyattahminproje\\geckodriver.exe')
+service = Service(executable_path='C:\\Users\\MONSTER\\Desktop\\geckodriver.exe')
 firefox_options = webdriver.FirefoxOptions()
 driver = webdriver.Firefox(service=service, options=firefox_options)
 firefox_options.add_argument("--headless")
@@ -27,7 +27,7 @@ while counter < 20:
         )
         element.click()
         time.sleep(3)  # Yeterli zaman ekleyin
-        driver.back()  # Geri dön
+        driver.back()  # Geri d?n
         time.sleep(3)
         counter += 1
         if (counter == 7):
